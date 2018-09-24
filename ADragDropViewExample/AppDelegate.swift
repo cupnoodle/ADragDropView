d48f8323f602170d894b8e62e4d250a8e6b0ac33
Axel <soulchildsince2013@gmail.com>
2018-09-24 23:26:33 +0800

    Initial Commit

---
+++ ADragDropViewExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ADragDropViewExample
+//
+//  Created by Soulchild on 24/09/2018.
+//  Copyright © 2018 fluffy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ADragDropViewExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ADragDropViewExample
+//
+//  Created by Soulchild on 24/09/2018.
+//  Copyright © 2018 fluffy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
